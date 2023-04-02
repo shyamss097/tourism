2c444b03ad93b322a09097699608f0e5e30ffe26
Shyam Sundar S <shyamss097@gmail.com>
2023-04-02 21:46:46 +0530

    first draft code

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class CustomUser(AbstractUser):
@@ -15,26 +16,6 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
-
-
-
-
-
-class Package(models.Model):
-   
-
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    source = models.CharField(max_length=100)
-    start_date = models.DateField()
-    duration = models.PositiveIntegerField()
-    manager = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
 class Food(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -52,15 +33,60 @@ class Accommodation(models.Model):
     def __str__(self):
         return self.name
 
+class Package(models.Model):
+   
 
-
-
-class Booking(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    package = models.ForeignKey(Package, on_delete=models.CASCADE)
-    food = models.ForeignKey(Food, on_delete=models.CASCADE)
-    accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
-    booking_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    source = models.CharField(max_length=100)
+    start_date = models.DateField()
+    duration = models.PositiveIntegerField()
+    manager = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    foods = models.ManyToManyField(Food)
+    accommodations = models.ManyToManyField(Accommodation)
 
     def __str__(self):
-        return f"{self.user.user.username} - {self.package.name}"
+        return self.name
+    
+    def get_total_food_cost(self, duration):
+        meals_per_day = 3
+        total_meals = meals_per_day * duration
+        total_food_cost = sum([food.price for food in self.foods.all()]) * total_meals
+        return total_food_cost
+
+    def get_total_accommodation_cost(self, duration):
+        total_accommodation_cost = sum([accommodation.price for accommodation in self.accommodations.all()]) * duration
+        return total_accommodation_cost
+
+    def get_total_cost(self, duration):
+        total_food_cost = self.get_total_food_cost(duration)
+        total_accommodation_cost = self.get_total_accommodation_cost(duration)
+        total_cost = self.price + total_food_cost + total_accommodation_cost
+        return total_cost
+
+class CartItem(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    duration = models.PositiveIntegerField(default=1)
+    food = models.ManyToManyField(Food, blank=True)
+    accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE, blank=True, null=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+
+    def __str__(self):
+        return f"{self.package.name}"
+
+class Cart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='cart')
+    items = models.ManyToManyField(CartItem)
+    date_added = models.DateTimeField(null=True,auto_now_add=True)
+    last_modified = models.DateTimeField(null=True,auto_now=True)
+
+
+    
+class Order(models.Model):
+    order_id = models.CharField(max_length=50, unique=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CustomUserCreationForm
 from .models import *
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def homepage(request):
     return render(request, 'base.html')
@@ -48,3 +49,158 @@ def login_view(request):
     else:
         return render(request, 'login.html')
 
+
+def package_list(request):
+    packages = Package.objects.all()
+    return render(request, 'package_list.html', {'packages': packages})
+
+
+
+def package_detail(request, pk):
+    package = get_object_or_404(Package, pk=pk)
+
+    # get all accommodations and their prices
+    accommodations = Accommodation.objects.all().values('id', 'name', 'price')
+    # get all foods and their prices
+    foods = Food.objects.all().values('id', 'name', 'price')
+
+    if request.method == 'POST':
+        # get selected accommodation and its price
+        accommodation_id = request.POST.get('accommodation')
+        if accommodation_id:
+            accommodation = get_object_or_404(Accommodation, pk=accommodation_id)
+            accommodation_price = accommodation.price
+        else:
+            accommodation = None
+            accommodation_price = 0
+
+        # get selected food and its price
+        food_id = request.POST.get('food')
+        if food_id:
+            food = get_object_or_404(Food, pk=food_id)
+            food_price = food.price
+        else:
+            food = None
+            food_price = 0
+
+        # calculate total cost
+        total_duration = package.duration
+        total_food_cost = total_duration * food_price * 3
+        total_accommodation_cost = total_duration * accommodation_price
+
+        # render template with selected options and total cost
+        return render(request, 'travel/package_detail.html', {
+            'package': package,
+            'accommodations': accommodations,
+            'foods': foods,
+            'selected_accommodation': accommodation,
+            'selected_food': food,
+            'total_food_cost': total_food_cost,
+            'total_accommodation_cost': total_accommodation_cost,
+            'total_cost': package.price + total_food_cost + total_accommodation_cost,
+        })
+
+    # render template with all options and no selections
+    return render(request, 'package_detail.html', {
+        'package': package,
+        'accommodations': accommodations,
+        'foods': foods,
+        'selected_accommodation': None,
+        'selected_food': None,
+        'total_food_cost': 0,
+        'total_accommodation_cost': 0,
+        'total_cost': package.price,
+    })
+
+
+def add_to_cart(request, package_id):
+    # Retrieve the package
+    package = get_object_or_404(Package, pk=package_id)
+
+    # Get the selected food and accommodation types from the form
+    selected_foods = request.POST.getlist('food')
+    selected_accommodation_id = request.POST.get('accommodation')
+    selected_accommodation = get_object_or_404(Accommodation, pk=selected_accommodation_id)
+
+    # Calculate the total cost of the package based on the selected options
+    duration = int(request.POST.get('duration'))
+    selected_foods = [food for food in selected_foods if isinstance(food, Food)]
+    food_cost = sum([food.price for food in selected_foods]) * duration * 3  # 3 meals per day
+    accommodation_cost = selected_accommodation.price * duration
+    total_price = package.price + food_cost + accommodation_cost
+
+    # Create a new cart item and add the selected food and accommodation types
+    cart_item = CartItem(package=package, duration=duration, accommodation=selected_accommodation, total_price=total_price,  user=request.user)
+    cart_item.save()
+    cart_item.food.set(selected_foods)  # Use set() to add selected foods to the cart item
+
+    
+
+    # Redirect to the cart page
+    return redirect('view-cart')
+
+def view_cart(request):
+    cart_items = CartItem.objects.filter(cart__user=request.user)
+    
+   
+    context = {'cart_items': cart_items}
+    return render(request, 'cart.html', context)
+
+def get_cart_total_price(cart):
+    total_price = 0
+    for item in cart.cartitem_set.all():
+        total_price += item.total_price
+    return total_price
+
+# @login_required
+# def checkout(request, pk):
+#     package = get_object_or_404(Package, pk=pk)
+#     cart = request.user.cart
+#     duration = package.duration
+#     selected_foods = cart.food.all()
+#     selected_accommodation = cart.accommodation
+#     food_cost = sum([food.price for food in selected_foods]) * duration * 3  # 3 meals per day
+#     accommodation_cost = selected_accommodation.price * duration
+#     total_price = package.price + food_cost + accommodation_cost
+#     order = Order.objects.create(user=request.user, package=package, total_price=total_price)
+#     cart.delete()
+#     return render(request, 'checkout.html', {'order': order})
+
+@login_required
+def checkout(request, package_id):
+    user = request.user
+    cart = user.cart
+
+    if not cart.items.exists():
+        return redirect('view-cart')
+
+    package = get_object_or_404(Package, id=package_id)
+
+    food_cost = sum([item.food.price for item in cart.items.all()]) * package.duration * 3
+    accommodation_cost = sum([item.accommodation.price for item in cart.items.all()]) * package.duration
+
+    total_price = food_cost + accommodation_cost + package.price
+
+    # create the order
+    order = user.orders.create(package=package, total_price=total_price)
+
+    # save the cart items to the order
+    for item in cart.items.all():
+        order.items.create(package=item.package, duration=item.duration, food=item.food, accommodation=item.accommodation, total_price=item.total_price)
+
+    # clear the cart
+    cart.items.all().delete()
+
+    # render the ticket template
+    return render(request, 'ticket.html', {'order': order})
+
+@login_required
+def remove_from_cart(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id, cart__user=request.user)
+    if request.method == 'POST':
+        cart_item.delete()
+        return redirect('view-cart')
+    context = {
+        'cart_item': cart_item
+    }
+    return render(request, 'remove_from_cart.html', context)
